9112340df400de69f9f727757125f124c24b3aa7
ShiniShiho <doquangminh28@gmail.com>
2023-06-13 10:20:38 +0700

    build structure, get templates

---
+++ main.py
@@ -0,0 +1,15 @@
+import subprocess
+import threading
+import configparser
+import resource.pixelColor as pixelColor
+
+def init():
+    
+        
+def main():
+       
+    
+                
+if __name__ == '__main__':
+    init()
+    main()
